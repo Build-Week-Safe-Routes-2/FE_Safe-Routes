6f96d5e9085867614642ff95cd7490fcd18ee646
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-23 13:50:04 -0500

    Setup component folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
 import "./App.css";
+
+import LoginForm from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Safe Routes</h1>
+
+			<LoginForm/>
     </div>
   );
 }
