7e1a50bad88a923fd3f3cd6554eda0568ce52ff3
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-09-24 16:48:02 -0500

    Not sure what has changed, saved file

--- src/App.js
+++ src/App.js
@@ -7,15 +7,9 @@ import Map from './components/Map/Map';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
      {/* <SigninForm /> */}
 			{/* <SignUpForm /> */}
 			<Map />
-=======
-      <h1>Safe Routes</h1>
-     <SigninForm />
-      {/* <SignUpForm /> */}
->>>>>>> f308b32051997203fea04234b4b9e9c430e863ab
     </div>
   );
 }
