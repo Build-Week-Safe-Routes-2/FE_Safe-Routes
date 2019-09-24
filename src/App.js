bef4113ada642cdf7b04dce76302c2f7901cf35b
gebhartn <nicholas.gebhart@gmail.com>
2019-09-24 09:16:02 -0700

    Switching to newer Map package
    react-google-maps is deprecated and uses classical components that throw errors to the console.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import "./App.css";
 
 import SignUpForm from "./components/SignUp/SignUp";
 import SigninForm from "./components/Signin/SigninForm";
-import Map from "./components/GoogleMaps/Map";
 const token = "AIzaSyCCdxVw0N2ydpKJ_yOm3VQgQzpq4rtSgBE";
 
 function App() {
   return (
     <div className="App">
       <h1>Safe Routes</h1>
-      <div style={{ width: "100vw", height: "100vh" }}>
-        <Map
-          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${token}`}
-          loadingElement={<div style={{ height: "100%" }} />}
-          containerElement={<div style={{ height: "100%" }} />}
-          mapElement={<div style={{ height: "100%" }} />}
-        />
-      </div>
-      {/* <SigninForm /> */}
+      <SigninForm />
       {/* <SignUpForm /> */}
     </div>
   );
