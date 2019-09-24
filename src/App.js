b57aa1b12c7f6bb1d3b52327b3c0e771e3db7643
gebhartn <nicholas.gebhart@gmail.com>
2019-09-24 10:43:03 -0700

    Removed diff from App.js
    Just some clean up

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from "react";
 import "./App.css";
 
 import SignUpForm from "./components/SignUp/SignUp";
-import SigninForm from './components/Signin/SigninForm'
-import Map from './components/Map/Map';
+import SigninForm from "./components/Signin/SigninForm";
+import Map from "./components/Map/Map";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     {/* <SigninForm /> */}
-			{/* <SignUpForm /> */}
-			<Map />
-=======
       <h1>Safe Routes</h1>
-     <SigninForm />
+      <SigninForm />
       {/* <SignUpForm /> */}
->>>>>>> f308b32051997203fea04234b4b9e9c430e863ab
     </div>
   );
 }
