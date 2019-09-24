fe1a5916e2b42b71d2638f34edf884b753a9ca3f
gebhartn <nicholas.gebhart@gmail.com>
2019-09-24 14:08:54 -0700

    State management for Map
    Initialized state for default center and zoom values
    Add Geocode to pass in search location from user input

--- src/components/Map/MapMarker.jsx
+++ src/components/Map/MapMarker.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const Marker = styled.div`
+  width: 15px;
+  height: 15px;
+  background: red;
+  border-radius: 100%;
+`;
 
 const MapMarker = props => {
   const { name } = props;
   return (
     <div>
-      <h1>{name}</h1>
+      <Marker
+        onClick={() => {
+          console.log(name);
+        }}
+      ></Marker>
     </div>
   );
 };
