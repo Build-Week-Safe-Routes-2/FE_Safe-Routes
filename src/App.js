69a36f2d25529f36348003939de30711d10a60e3
gebhartn <nicholas.gebhart@gmail.com>
2019-09-23 18:36:10 -0700

    Added Google Map
    Boilerplate Component for Google Maps
    
    defaultCenter should load from user locale
    Map has inline default styling, must edit containers or create classNames
    Must add marker integration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./App.css";
 
 import SignUpForm from "./components/SignUp/SignUp";
-import SigninForm from './components/Signin/SigninForm'
+import SigninForm from "./components/Signin/SigninForm";
+import Map from "./components/GoogleMaps/Map";
+const token = "AIzaSyCCdxVw0N2ydpKJ_yOm3VQgQzpq4rtSgBE";
 
 function App() {
   return (
     <div className="App">
       <h1>Safe Routes</h1>
-     <SigninForm />
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <Map
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${token}`}
+          loadingElement={<div style={{ height: "100%" }} />}
+          containerElement={<div style={{ height: "100%" }} />}
+          mapElement={<div style={{ height: "100%" }} />}
+        />
+      </div>
+      {/* <SigninForm /> */}
       {/* <SignUpForm /> */}
     </div>
   );
