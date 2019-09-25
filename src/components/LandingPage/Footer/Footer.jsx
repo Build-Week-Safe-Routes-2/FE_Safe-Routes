a97b42520c1e0824d3933ee36fb6bd9392e2cc04
gebhartn <nicholas.gebhart@gmail.com>
2019-09-25 13:30:21 -0700

    Adjusted file structure
    Corrected pathing on scss imports

---
+++ src/components/LandingPage/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../reset.scss";
+import "../../../index.scss";
+import "./footer.scss";
+
+const Footer = () => {
+  return <h1>Hello Footer</h1>;
+};
+
+export default Footer;
--- src/components/LandingPage/Hero/Hero.jsx
+++ src/components/LandingPage/Hero/Hero.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import "./variables.scss";
-import "./reset.scss";
+import "../../reset.scss";
 import "../../../index.scss";
-import "./page-specific.scss";
 
 const Hero = props => {
   return <h1>Hello World</h1>;
