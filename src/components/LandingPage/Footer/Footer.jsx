74ccb06f2ac144a30a60376654db1d5e3a8936c1
gebhartn <nicholas.gebhart@gmail.com>
2019-09-25 13:59:40 -0700

    Added skeleton for footer
    Skeleton of footer is complete, needs links and copy

--- src/components/LandingPage/Footer/Footer.jsx
+++ src/components/LandingPage/Footer/Footer.jsx
@@ -2,9 +2,50 @@ import React from "react";
 import "../../reset.scss";
 import "../../../index.scss";
 import "./footer.scss";
+import bus from "./assets/undraw_bus_stop_h370.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <h1>Hello Footer</h1>;
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-container">
+        <div className="footer-hero">
+          <div className="logo-container">
+            <img />
+            <h3>Safe Routes</h3>
+          </div>
+          <div className="story-container">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos praesentium in dicta at beatae quo ullam, minus
+              necessitatibus! Aliquam fugit ipsam a maxime doloremque, sint amet
+              incidunt obcaecati animi cum.
+            </p>
+          </div>
+          <div className="footer-links-container">
+            <i>a</i>
+            <i>b</i>
+            <i>c</i>
+          </div>
+          <div className="footer-nav-container">
+            <nav className="left-nav">
+              <Link className="footer-link">About</Link>
+              <Link className="footer-link">Contact</Link>
+              <Link className="footer-link">FAQ</Link>
+              <Link className="footer-link">GitHub</Link>
+            </nav>
+            <nav className="right-nav">
+              <Link className="footer-link">Login</Link>
+              <Link className="footer-link">Sign Up</Link>
+              <Link className="footer-link">Updates</Link>
+              <Link className="footer-link">Blog</Link>
+              <Link className="footer-link">Mailing List</Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
